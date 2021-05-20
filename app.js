057813c100028c2b9ac46d91f9d69f079d05b707
Kay <69727381+hallokay@users.noreply.github.com>
2021-05-20 10:38:40 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -19,15 +19,13 @@ class Calculator {
     }
 
     appendNumber(number) {
-        if(number == '.' && this.currentOperand.includes('.'))
-            return
+        if(number == '.' && this.currentOperand.includes('.')) return
 
         this.currentOperand = this.currentOperand.toString() + number.toString();
     }
 
     chooseOperation(operation) {
-        if(this.currentOperand === '') 
-            return
+        if(this.currentOperand === '') return
 
         if(this.prevOperand !== '') {
             this.compute();
@@ -44,8 +42,7 @@ class Calculator {
             current = parseFloat(this.currentOperand);
         
 
-        if( isNaN(prev) || isNaN(current))
-            return
+        if( isNaN(prev) || isNaN(current)) return
 
         switch (this.operation) {
             case '+':
@@ -78,28 +75,39 @@ class Calculator {
 
     getDisplayNumber (number) {
         const stringNum = number.toString();
-        const integerDigits = parseFloat(stringNum.split('.'))
-        const floatNum = parseFloat(number);
-        // parseFloat 문자열을 실수로 바꿈 parseFloat( string )
-        if(isNaN(floatNum)) return '';
-        return number.toLocaleString('en');
-        // 사용자의 문화권에 맞는 시간표기법으로 년,월,일 시간을 리턴
-        // * toLocaleString 은 Number 타입의 내장 함수이며, 
-        // 인자로 들어온 지역 값에 따른 숫자 표기 방식을 적용하여, 문자열로 반환하는 역할을 합니다. 'en'은 영어권 국가의 숫가 표기 방식을 사용하겠다는 것을 지정한 것입니다. 미국이나 영어권 국가에서는 숫자를 표기할 때, 3자리마다 숫자를 끊어 콤마를 삽입하여 사용합니다.
+        const integerDigits = parseFloat(stringNum.split('.')[0]);
+        const decimalDigits = stringNum.split('.')[1];
+        let integerDisplay;
+        if(isNaN(integerDigits)) {
+            integerDisplay = '';
+        }else {
+            integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 });
+        }    
 
+        if(decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+
+        // // parseFloat 문자열을 실수로 바꿈 parseFloat( string )
+        // if(isNaN(floatNum)) return '';
+        // return number.toLocaleString('en');
+        // // 사용자의 문화권에 맞는 시간표기법으로 년,월,일 시간을 리턴
+        // // * toLocaleString 은 Number 타입의 내장 함수이며, 
+        // // 인자로 들어온 지역 값에 따른 숫자 표기 방식을 적용하여, 문자열로 반환하는 역할을 합니다. 'en'은 영어권 국가의 숫가 표기 방식을 사용하겠다는 것을 지정한 것입니다. 미국이나 영어권 국가에서는 숫자를 표기할 때, 3자리마다 숫자를 끊어 콤마를 삽입하여 사용합니다.
+        //   decimal - 소수 integer - 정수 digits - 숫자
     }
 
     updateDisplay() {
         this.currentOperandTextEl.innerText = this.getDisplayNumber(this.currentOperand);
-        if(this.operation !== null){
+        if(this.operation != null){
             this.prevOperandTextEl.innerText = 
-                `${getDisplayNumber(this.prevOperand)} ${this.operation}`;
-        }
-        
+                `${this.getDisplayNumber(this.prevOperand)} ${this.operation}`;
+        } else {
+            this.prevOperandTextEl.innerText = '';
+        }   
     }
-
-   
-
 }
  
  
@@ -148,3 +156,6 @@ deleteBtn.addEventListener('click', btn => {
     calculator.updateDisplay();
 });
 
+
+
+// operation -조작
